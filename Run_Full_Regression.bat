49248d3ee5f200c0fb1575eec701e42f438cc008
AnnaNodel <anna.nodelman@netformx.com>
2022-03-13 08:32:33 +0200

    changes

--- Run_Full_Regression.bat
+++ Run_Full_Regression.bat
@@ -4,5 +4,8 @@ C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Dash
 C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Accounts_Screen.py
 C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Interactions_Screen.py
 C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Interactions_CRUD.py
+C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Alerts_Screen.py
 C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Alerts_CRUD.py
+C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Configuration_HealthScore.py
+C:\Python39\python.exe C:/Users/annano/PycharmProjects/CSMAutomationProject/Configuration_Playbooks.py
 pause
